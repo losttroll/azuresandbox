51e3737f268dbae514dd8550e0dab07620dc4d1a
Ben <benjgertz@gmail.com>
2024-09-19 14:32:13 -0600

    Update deploysandbox.ps1
    revamped with functions

--- deploysandbox.ps1
+++ deploysandbox.ps1
@@ -1,87 +1,156 @@
-cls
-#Azure Sandbox Deployment Script
-Write-Host "Building Sandbox"
+function CreateDir{
+    #Will check for a directory and create it if it doesn't exists
+    param(
+        [string]$destination
+        
+    )
+    
+    #Create Downloads Folder
+    if (-Not (Test-Path $destination)) {
+        Write-Host "Creating Directory: $destination"
+        $a= New-Item -Type Directory $destination
+       }
 
-#Create Downloads Folder
-if (-Not (Test-Path c:\\td)) {
-    Write-Host "Creating td folder"
-    New-Item -Type Directory -Path 'c:\\' -Name td -Force
+    }
+
+function extractZip{
+param(
+        [string]$url,
+        [string]$targetpath,
+        [string]$product,
+        [string]$filename,
+        [string]$sourcedir
+        )
+
+    $targetfile = "$sourcedir\\$filename"
+    #Extract SysInternals
+    if (-Not (Test-Path  $targetpath)) {
+        write-host "Extracting $targetfile to: $targetpath"
+        Expand-Archive -LiteralPath $targetfile -DestinationPath $targetpath
+}
+}
+  
+function DownloadFile{
+    param(
+        [string]$url,
+        [string]$downloadpath,
+        [string]$product,
+        [string]$filename
+    )
+    #Create destination directory
+    CreateDir -destination $downloadpath
+    
+    $destination = "$downloadpath\\$filename"
+           
+    if (-Not (Test-Path $destination)) {
+        Write-Host "[*] Downloading $product"
+        Write-Host "    Url:  $url"
+        Write-Host "    Path: $destination"
+
+        $wc = New-Object net.webclient
+        $wc.Downloadfile($url, $destination)
+
+        Write-Host "[+] Downloaded"
+        }
+    else {
+       Write-Host "[*] Already downloaded"
+    }
+  
+}
+
+
+function InstallProduct{
+    param(
+        [string]$filename,
+        [string]$sourcelocation,
+        [string]$product,
+        [string]$install_args,
+        [string]$checkpath
+    )
+    $installfile = "$sourcelocation\\$filename"
+    
+    
+    if (-Not (Test-Path checkpath)) {
+        Write-Host "[*] Installing $product"
+        Write-Host "    Install command: $installfile $install_args"
+        Start-Process -FilePath $installfile -ArgumentList $install_args
+        Write-Host "[+] Install Command Executed"
+        }
+    else {
+       Write-Host "[*] Already downloaded"
+    }
+    
     
 }
 
+cls
+# Download and Import the support files CSV
+$template = "default_win10.csv"
+$template_download = "$env:TEMP\$template"
+Invoke-WebRequest "https://raw.githubusercontent.com/losttroll/azuresandbox/main/$template" -OutFile $template_download
 
-#Download Burpsuite
-if (-Not (Test-Path c:\\td\\burpsuite.exe)) {
-    Write-Host "Downloading Burpsuite Community"
-    invoke-webrequest -uri 'https://portswigger-cdn.net/burp/releases/download?product=community&version=2024.6.6&type=WindowsX64' -OutFile 'c:\\td\\burpsuite.exe'
-}
+$csvData = Import-Csv -Path $template_download
+$file_destination = "c:\\td1"
 
-#Install Burpsuite
-if (-Not(Test-Path $env:USERPROFILE\AppData\Local\BurpSuiteCommunity\BurpSuiteCommunity.exe)) {
-   Write-Host "Installing Burpsuite Community"
-   c:\\td\\burpsuite.exe -q -c
-}
+# Iterate through each row in the CSV
+foreach ($row in $csvData) {
+    $download_folder = "c:\\td1"
 
-#Download Notepad++
-if (-Not (Test-Path c:\\td\\notepadplusplus.exe)) {
-    Write-Host "Downloading Notepad++"
-    invoke-webrequest -uri 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.7/npp.8.6.7.Installer.x64.exe' -OutFile 'c:\\td\\notepadplusplus.exe'
-}
+    # Access columns by their header names
+    $product = $row.product
+    $install_path = $row.install_path
+    $file_name = $row.install_file_name
+    $mode = $row.mode
+    $override_destination = $row.override_file_destination
+    $operation = $row.operation
+    $install_args = $row.install_args
+    $operation_location = $row.operation_location
 
-#Install Notepad++
-if (-Not(Test-Path "C:\\Program Files\\Notepad++\\notepad++.exe")) {
-   Write-Host "Installing Notepad++"
-   c:\\td\\notepadplusplus.exe /S
-}
+    #Handle file path for configuration files in installation
+     if ($install_args.Contains("FILEDESTINATION")) {
+        
+        $install_args = $install_args.Replace("FILEDESTINATION", $download_folder)
+        write-host "Install Path Updated to:  $install_args"
+        }
 
-#Sysinternals link - https://download.sysinternals.com/files/SysinternalsSuite.zip
+    #Handle file path for local install paths
+     if ($install_path.Contains("FILEDESTINATION")) {
+        
+        $install_path = $install_path.Replace("FILEDESTINATION", $download_folder)
+        write-host "Install Arguments Updated to:  $install_path"
+        }
 
+    #overside source
+    if ($override_destination.Length -gt 1) {
+        write-host "Overriding Source"
+        if ($override_destination.Contains("FILEDESTINATION")) {
+        
+        $override_destination = $override_destination.Replace("FILEDESTINATION", $download_folder)
+        write-host "Source Files updated to:  $download_folder"
+        }
 
-#Download SysInternals
-if (-Not (Test-Path c:\\td\\SysinternalsSuite.zip)) {
-    Write-Host "Downloading SysInternals"
-    invoke-webrequest -uri 'https://download.sysinternals.com/files/SysinternalsSuite.zip' -OutFile 'c:\\td\\SysinternalsSuite.zip'
-}
+        #write-host  1, $file_destination
+        #write-host 2, $override_destination
+        $download_folder = $override_destination
+    }
+     
 
-#Extract SysInternals
-if (-Not (Test-Path  "c:\\td\\SysInternals")) {
-    write-host "Extracting Sysinternals to: c:\\td\\SysInternals"
-    Expand-Archive -LiteralPath 'c:\\td\\SysinternalsSuite.zip' -DestinationPath c:\\td\\SysInternals\\SysInternals
-}
+    #Download or copy file
+    if ($mode -in @("install", "extract", "download")) {
+        if ($operation -eq "web_download") {
+        DownloadFile -url $operation_location -downloadpath $file_destination -filename $file_name -product $product
+            }
+        }
 
-#Extract SysInternals
-if (-Not (Test-Path  "$env:USERPROFILE\\Desktop\\SysInternals")) {
-    write-host "Copying Sysinternals to: $env:USERPROFILE\SysInternals"
-    Copy-Item -Recurse -LiteralPath 'c:\\td\\SysInternals' -Destination $env:USERPROFILE\Desktop\\
-}
+    #Extract file
+    if ($mode -in @("extract")) {
+        extractZip -targetpath $install_path -product $product -filename $file_name -sourcedir $file_destination
 
-#https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml
+        }  
+    
+    #Install file
+    if ($mode -in @("install")) {
+         InstallProduct -filename $file_name -sourcelocation $file_destination -product $product -install_args $install_args -checkpath $install_path
+         }
 
-#Download Sysmon Config
-if (-Not (Test-Path c:\\td\\sysmonconfig.xml)) {
-    Write-Host "Downloading Sysmon Config"
-    invoke-webrequest -uri 'https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml' -OutFile c:\\td\\sysmonconfig.xml
-}
-
-#Install Sysmon
-if (-Not (Test-Path C:\\Windows\\sysmon.exe)) {
-    Write-Host "Installing Sysmon"
-    C:\td\SysInternals\SysInternals\\Sysmon.exe -i c:\td\sysmonconfig.xml
-}
-
-
-#https://7-zip.org/a/7z2408-x64.msi
-#Download Sysmon Config
-if (-Not (Test-Path c:\\td\\7zip.msi)) {
-    Write-Host "Downloading 7zip"
-    invoke-webrequest -uri 'https://7-zip.org/a/7z2408-x64.msi' -OutFile c:\\td\\7zip.msi
-}
-
-#Install 7zip
-if (-Not (Test-Path "C:\\Program Files\\7-Zip\\7z.exe")) {
-    Write-Host "Installing 7zip"
-     C:\td\7zip.msi /quiet
-}
-
-
-write-host Completed
+    }
